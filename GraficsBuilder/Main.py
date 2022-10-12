a1e197d18f9fbbe993ed28f137649b49d50d41b5
Gleb Belous <peremichka256@gmail.com>
2022-10-12 15:03:24 +0700

    График строится прямо в форме с возможностью взаимодействия

--- GraficsBuilder/Main.py
+++ GraficsBuilder/Main.py
@@ -1,19 +1,25 @@
-# coding: utf-8
+import tkinter
 from tkinter import *
 from tkinter import ttk
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import pandas as pd
+matplotlib.use("TkAgg")
 
 
 def graphic_build_click():
     table = pd.read_excel('parabola.xlsx')
     x = table.values[:, 0]
     y = table.values[:, 1]
-
-    plt.figure(figsize = (7, 7))
+    graphic_figure = plt.figure(figsize = (7, 7))
+    graphic_figure.add_subplot(111)
+    canvas = FigureCanvasTkAgg(graphic_figure, root)
+    toolbar = NavigationToolbar2Tk(canvas, root)
+    toolbar.update()
+    canvas.get_tk_widget().pack()
     plt.plot(x, y)
-    plt.show()
 
 
 if __name__ == '__main__':
